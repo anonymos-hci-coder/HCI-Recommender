f4aefd11f4d3cb80c6167ed687b99d37dbf4242f
Anonymous <anon@anon.com>
2025-10-06 19:10:03 +0330

    Initial commit

---
+++ README.m
@@ -0,0 +1,11 @@
+To download the data, please do this step by step:
+1. From the source to the root of the project, please cd to '/data'
+2. Run the following code in the terminal:
+```
+curl -L -o data.zip https://files.grouplens.org/datasets/movielens/ml-32m.zip
+```
+3. Unzip the downloaded file:
+```
+unzip data.zip
+```
+4. Verify CSV files are extracted in `/data/ml-32m/`
